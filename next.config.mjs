8333b37314dd0b6d88434c2e15f70fed8da3dbf4
Jaromír Trnka <JrTrnka55@gmail.com>
2024-07-13 21:50:36 +0200

    Serwist
    - Added serwist based on the documentation

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
+import withSerwistInit from "@serwist/next";
 import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
+
+const withSerwist = withSerwistInit({
+    swSrc: "app/sw.ts",
+    swDest: "public/sw.js",
+});
+
 const nextConfig = {
     experimental: {
         turbo: {},
     },
 };
+
 export default MillionLint.next({
     rsc: true,
-})(nextConfig);
+})(withSerwist(nextConfig));
