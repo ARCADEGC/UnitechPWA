2cf3cad3a0d07613de9c1a51742ee14b9b8aedb4
Jaromír Trnka <JrTrnka55@gmail.com>
2024-09-20 18:44:24 +0200

    new schema & types

--- types/dbSchemas.ts
+++ types/dbSchemas.ts
@@ -1,5 +1,11 @@
-import { order, User } from "@/db/schema";
+import { order, User, OrderHeader, OrderNewPCK, OrderPP2 } from "@/db/schema";
 
 export type TUser = typeof User.$inferInsert;
 
 export type TOrder = typeof order.$inferInsert;
+
+export type TOrderHeader = typeof OrderHeader.$inferInsert;
+
+export type TOrderNewPCK = typeof OrderNewPCK.$inferInsert;
+
+export type TOrderPP2 = typeof OrderPP2.$inferInsert;
