88892e54c5cbf62dd261e7f753ff58c915c4b679
Jaromír Trnka <jaromir.trnka@protonmail.com>
2024-11-06 16:43:43 +0100

    https://github.com/drizzle-team/drizzle-orm/issues/680#issuecomment-1592557548

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,16 +1,6 @@
-import config from "@/drizzle.config";
 import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { migrate } from "drizzle-orm/vercel-postgres/migrator";
 
 import * as schema from "@/db/schema";
 
 export const db = drizzle(sql, { schema });
-
-async function main() {
-    await migrate(db, { migrationsFolder: config.out! });
-
-    // await sql.end();
-}
-
-main();
